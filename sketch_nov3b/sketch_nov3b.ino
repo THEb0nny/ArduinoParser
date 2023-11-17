b80f20166835cac3f4c6c9f7db4e4044007df38c
Dmitriy Antipov <aston_bonny@mail.ru>
2023-11-17 17:35:23 +0300

    17_11_23_1

--- sketch_nov3b/sketch_nov3b.ino
+++ sketch_nov3b/sketch_nov3b.ino
@@ -18,13 +18,12 @@ void ParseFromSerialInputValues(bool debug) {
     char inputStr[30]; // Массив символов для записи из Serial
     int amount = Serial.readBytesUntil(';', inputStr, 30); // Считать посимвольно до символа конца пакета точки с запятой и записать количество полученных байт в переменную
     inputStr[amount] = NULL; // Если отправляющее устройство не отправит нулевой символ, то он не запишется в буффер и вывод строк будет некорректным, решение дописать вручную и т.о. закрываем строку
-
     GParser data(inputStr, ','); // Парсим массив символов по символу запятой
     int am = data.split(); // Получаем количество данных, внимание, ломает строку!
-
     for (int i = 0; i < am; i++) {
       String tmpStr = data[i];
       tmpStr.replace(" ", ""); // Удалить пробел, если он был введёт по ошибке
+      tmpStr.trim(); // Удаление ведущими и конечные пробелы
       char tmpCharArr[tmpStr.length()];
       tmpStr.toCharArray(tmpCharArr, tmpStr.length() + 1);
       if (debug) Serial.println(String(i) + ") " + tmpStr); // Вывести начальную строку
